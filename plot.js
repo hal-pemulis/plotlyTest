1c41d44e8856535f24586ac84072df8bf45c8032
hal-pemulis <travisatyler@gmail.com>
2020-05-11 19:26:25 -0500

    Added files to github pages repo

---
+++ plot.js
@@ -0,0 +1,57 @@
+// # Seek and Deploy
+
+// In this activity, you will deploy a Plotly project with a local data file to GitHub Pages.
+
+// * You are given a Plotly visualization project, with `index.html`, `plot.js`, and `data.json`.
+
+// * Deploy the project to GitHub Pages.
+
+// ## Hint
+
+// Consult [https://pages.github.com/](https://pages.github.com/) for reference. Be sure to select the **Project Site** and **Start from Scratch** options for instructions.
+
+
+
+// Use d3.json() to fetch data from JSON file
+// Incoming data is internally referred to as incomingData
+(async function(){
+  var incomingData = await d3.json("data/data.json");
+  function filterMovieRatings(movie) {
+    return movie.imdbRating > 8.9;
+  }
+
+  // Use filter() to pass the function as its argument
+  var filteredMovies = incomingData.filter(filterMovieRatings);
+
+  //  Check to make sure your are filtering your movies.
+  console.log(filteredMovies);
+
+  // Use the map method with the arrow function to return all the filtered movie titles.
+  var titles = filteredMovies.map(movies =>  movies.title);
+
+  // Use the map method with the arrow function to return all the filtered movie metascores.
+  var ratings = filteredMovies.map(movies => movies.metascore);
+
+  // Check your filtered metascores.
+  console.log(ratings);
+
+  // Create your trace.
+  var trace = {
+    x: titles,
+    y: ratings,
+    type: "bar"
+  };
+
+  // Create the data array for our plot
+  var data = [trace];
+
+  // Define the plot layout
+  var layout = {
+    title: "The highest critically acclaimed movies.",
+    xaxis: { title: "Title" },
+    yaxis: { title: "Metascore (Critic) Rating"}
+  };
+
+  // Plot the chart to a div tag with id "bar-plot"
+  Plotly.newPlot("bar-plot", data, layout);
+})()
